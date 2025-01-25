043944ca509da3612949cc8caedf916f71c0080d
HarryY-Yao <[200~184984708+HarryY-Yao@users.noreply.github.com>
2025-01-25 16:56:24 -0500

    Create startTime, updatetime, and formatTime timer functions

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 const timerDisplay = document.querySelector(".time");
 const startBtn = document.querySelector(".start");
 
-let timeSeparated = timerDisplay.textContent.split(":");
+let timer;
+let minutes = 25;
+let seconds = 0;
+let isPaused = false;
+
+let startTime = function() {
+    timer = setInterval(updateTimer, 1000);
+}
+
+let updateTime = function() {
+    timerDisplay.textContent = formatTime(minutes, seconds);
+
+    if (minutes === 0 && seconds === 0) {
+        clearInterval(timer);
+        alert("Time's up! You deserve a break :)")
+    } else if (!isPaused) {
+        if (seconds > 0) {
+            seconds--;
+        } else {
+            seconds = 59;
+            seconds--;
+        }
+    }
+} 
